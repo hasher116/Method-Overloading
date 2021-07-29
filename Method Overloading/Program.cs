764c2751dbfe3f24211d6a64a2cddd79237690a4
hasher116 <andrei13081996@mail.ru>
2021-07-29 15:54:09 +0300

    Добавьте файлы проекта.

---
+++ Method Overloading/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Method_Overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            Calc clc = new Calc();
+            Console.WriteLine(clc.sum(6.4f, 4));
+            clc.incr(x);
+            clc.incr(x);
+            clc.incr(ref x);
+            clc.incr(ref x);
+        }
+    }
+
+    class Calc
+    {
+        public int sum (int x, int y)
+        {
+            return x + y;
+        }
+        public float sum(float x, int y)
+        {
+            return x + y;
+        }
+        public int sum(int x, int y, int z)
+        {
+            return x + y + z;
+        }
+
+        public void incr (int f)
+        {
+            f++;
+            Console.WriteLine(f);
+        }
+        public void incr (ref int f)
+        {
+            f++;
+            Console.WriteLine(f);
+        }
+    }
+}
